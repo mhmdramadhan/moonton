ac436ae28d41189d2c79eae4afa9f51b3a1bf7a4
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2024-03-07 10:34:28 +0700

    Add spatie

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+            'password' => Hash::make('123qweasd'),
+        ]);
+
+        $admin->assignRole('admin');
+    }
+}
