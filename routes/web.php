679fe6c9c135cba4cf29a216325174a974f45c62
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2024-03-14 05:58:17 +0700

    testing 2

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
-
+ 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
