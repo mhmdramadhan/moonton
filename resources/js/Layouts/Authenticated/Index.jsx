ab7d4d8f478bd1f75bf2c19d1c37145646c5be40
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2024-03-19 06:13:10 +0700

    Slicing Sidebar

--- resources/js/Layouts/Authenticated/Index.jsx
+++ resources/js/Layouts/Authenticated/Index.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import Sidebar from "./Sidebar";
 
 const Authenticated = ({ children }) => {
     return (
         <>
-            <div class="mx-auto max-w-screen hidden lg:block">
+            <div className="mx-auto max-w-screen hidden lg:block">
                 {/* START: sidebar */}
+                <Sidebar />
                 {/* END: sidebar */}
 
                 {/* START: content */}
-                <div class="ml-[300px] px-[50px]">
-                    <div class="py-10 flex flex-col gap-[50px]">
+                <div className="ml-[300px] px-[50px]">
+                    <div className="py-10 flex flex-col gap-[50px]">
                         {/* START: topbar */}
                         {/* END: topbar */}
 
@@ -18,8 +20,8 @@ const Authenticated = ({ children }) => {
                 </div>
                 {/* END: content */}
             </div>
-            <div class="mx-auto px-4 w-full h-screen lg:hidden flex bg-black">
-                <div class="text-white text-2xl text-center leading-snug font-medium my-auto">
+            <div className="mx-auto px-4 w-full h-screen lg:hidden flex bg-black">
+                <div className="text-white text-2xl text-center leading-snug font-medium my-auto">
                     Sorry, this page only supported on 1024px screen or above
                 </div>
             </div>
@@ -27,4 +29,4 @@ const Authenticated = ({ children }) => {
     );
 };
 
-export default Index;
+export default Authenticated;
